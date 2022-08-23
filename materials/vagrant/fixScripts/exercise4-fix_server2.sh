53e63f946413d87e319a0f6f84296b0072530c2e
gal-kerem <73199341+galke7@users.noreply.github.com>
2022-08-24 02:36:29 +0300

    ex4 ssh key injected

--- materials/vagrant/fixScripts/exercise4-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #add fix to exercise4-server2 here
 echo '192.168.60.10 server1' >> /etc/hosts
-#echo'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCvxRyyEkVH/k6SH+bId28OXnDbOQimxFrmsN7k/dEd3sWzsS9k5D64K30jMWXZtDuAAqsIAdAqvqJIGlhMp5cwpjlyW5RvAqnvVfV5zXpr1rtj7JjPqPg51XuycOOYvFLPJurqEq6/Pxj8IIOQnhlgRmiaFD7cJPz9QtIrtZdAXMqw7dHwfKJUi59Jszx35d3fUulwFgJeZ6Y8sgmFeoCAZklHBT/ZP89d2XU3CmewVf7IMDFqMxBf3ykj8OrvrgMFoBlDDHDOmtpQIgNpvl+B785Ff7DWjWlN8t97PsLhbztjPjnSJt/p0LZwvY8SVMpX0Rk9i1+CCH0lU6PlbIXWKJBya1UfqnS+dHJS0hoAmINUSr/9/fgflMgbUWGIDL7tnRa9qn8kYPpNLE73Az6Yw1bclACp9Te0Hz+WlJQErGtfqRuK4Ls3xAH1cxl7pYYc82RBMD0X0BmvU4MSyCcYraA6T2LkeDIEDJ4yO59lOnK0bd2vdy64BpIJKgmexcc= vagrant@server1' >> ~/.ssh/authorized_keys
-ssh-keygen -t rsa -N "" -f /home/vagrant/.ssh/id_rsa
-ssh-copy-id vagrant@192.168.60.10
+echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDRa3f1kBTRpdmuMP7qtKUjdnPngdx5ZSX6yjZTskVY+Ws1UGiiAnLqXrPB/KQKE5Web0i7fSw809RUpIHMbw69BwFOQeqQXc0hNp+ZyyUqEUc9QM0Wwn4JWnMqXSYp+FV2H3F6+flHUIvBbAOgKp4Wd1PNHt7HoPIT3pCiQC1XgR3TdhsW5gt2gr8ULpGkzjcImflBCDRuhBvrjsVEiEraQdLo+eUIv+91WqLkkczKxstmro7VNx07Hn7lxjn85Sh4m5JNavUN/8jYofrTtyTxhWtcaZX43BQLL4FuEdiREr9BdAa8TAYxgCsl+kfT9dQssx4q4VyqN9QSIRT/ijiXnxfSMSxooPZrulmZQXNuV+NLMCyFD1XOkHFZFNsE+msUWa/ZblkKPiOo35X10uewJL6zZLEOppvUuKXYgBuHtGMOkCNFyjLhbnNc+76zeHd4mxNbB07vGiJNbfd3LKofyuHuTLcSYz88K+LCwMxjS8CJgYgL2aXdWhYIki+/tBc= vagrant@server1' >> /home/vagrant/.ssh/authorized_keys
+
+
+#ssh-keygen -t rsa -N "" -f /home/vagrant/.ssh/id_rsa
+#ssh-copy-id vagrant@192.168.60.10
+
+
