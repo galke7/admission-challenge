5364033321afed43de1a7c6ce47137154d2fa1fa
gal-kerem <73199341+galke7@users.noreply.github.com>
2022-08-28 00:42:23 +0300

    ex6 Added support for passing multiple file names by storing arguments in an arrey

--- materials/vagrant/fixScripts/exercise6-fix.sh
+++ materials/vagrant/fixScripts/exercise6-fix.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 #add fix to exercise6-fix here
 
-file1=$1
-file2=$2
-dir=$3
+#Set all arguments to arrey
+ARGS=("$@")
 
-myhost=$(hostname)
+#Set last argument to var DIR
+DIR="${ARGS[-1]}"
 
-if [ "$myhost" == "server1" ]
+#Remove DIR from arrey
+unset ARGS[-1]
+
+MYHOST=$(hostname)
+
+if [ "$MYHOST" == "server1" ]
 then
-  connectTo="server2"
+  TARGET="server2"
 else
-  connectTo="server1"
+  TARGET="server1"
 fi
 
-#echo "copying files to" $connectTo
-#echo from $myhost
-
-rsync -e ssh -v $1 $2 $connectTo:$3 > $HOME/rsync.log 2>/dev/null
+rsync -e ssh -v ${ARGS[@]} $TARGET:$DIR > $HOME/rsync.log 2>/dev/null
 grep "total" $HOME/rsync.log | awk '{print $4}'
