c3880ff771613aaf0676a587379d256ce840ba8a
gal-kerem <73199341+galke7@users.noreply.github.com>
2022-08-27 23:12:25 +0300

    ex6 - copy 2 files to different server and print files size

--- materials/vagrant/fixScripts/exercise6-fix.sh
+++ materials/vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+
+file1=$1
+file2=$2
+dir=$3
+
+myhost=$(hostname)
+
+if [ "$myhost" == "server1" ]
+then
+  connectTo="server2"
+else
+  connectTo="server1"
+fi
+
+#echo "copying files to" $connectTo
+#echo from $myhost
+
+rsync -e ssh -v $1 $2 $connectTo:$3 > $HOME/rsync.log 2>/dev/null
+grep "total" $HOME/rsync.log | awk '{print $4}'
